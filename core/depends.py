f923d9107b3cda4c6872918cb89c9e89c5c55e15
zhyipeng <zhyipeng@outlook.com>
2022-01-05 17:47:48 +0800

    verify user

--- core/depends.py
+++ core/depends.py
@@ -1,17 +1,26 @@
+from asyncio import create_task
+
 from fastapi import Header
 
 from consts.common import RedisKeys
 from core.exceptions import AuthenticationError
+from core.settings import settings
 from utils.redis_helper import redis_cli
 
 
-async def verify_user(x_token: str = Header(...)):
+async def verify_user(x_token: str = Header(...)) -> int:
     if not x_token:
         raise AuthenticationError
 
     key = f'{RedisKeys.AUTH_TOKEN_PREFIX}{x_token}'
-    uid = redis_cli.get(key)
+    uid = await redis_cli.get(key)
     if not uid:
-        raise AuthenticationError('Token 已过期')
+        raise AuthenticationError(error_message='Token 已过期')
 
-    return uid
+    create_task(refresh_token(x_token))
+    return int(uid)
+
+
+async def refresh_token(token: str):
+    key = f'{RedisKeys.AUTH_TOKEN_PREFIX}{token}'
+    await redis_cli.expire(key, settings.TOKEN_EXPIRE)
