b202a476594acc8212bc9c1a9346748f4c85b9f1
zhyipeng <zhyipeng@outlook.com>
2022-01-07 15:20:56 +0800

    cors

--- core/middlewares.py
+++ core/middlewares.py
@@ -1,1 +1,11 @@
-from fastapi import Request, Response
+from fastapi import FastAPI
+
+from fastapi.middleware.cors import CORSMiddleware
+
+
+def use_middlewares(app: FastAPI):
+    app.add_middleware(CORSMiddleware,
+                       allow_origins=['*'],
+                       allow_credentials=True,
+                       allow_methods=['*'],
+                       allow_headers=['*'])
--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import asyncio
 from fastapi import FastAPI
 
 from core.exceptions import register_exception_handlers
+from core.middlewares import use_middlewares
 from views import router
 
 app = FastAPI()
 app.include_router(router)
 
+use_middlewares(app)
 register_exception_handlers(app)
 
 
